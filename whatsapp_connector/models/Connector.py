41185935decabd2b84c9792d393c80d7ee3b0a67
Juan Camilo Muñoz Rodríguez <132085703+Jcmr5720@users.noreply.github.com>
2025-08-25 09:06:02 -0500

    Avoid sending body on status logout

--- whatsapp_connector/models/Connector.py
+++ whatsapp_connector/models/Connector.py
@@ -457,7 +457,7 @@ class AcruxChatConnector(models.Model):
     def hook_request_args(self, args):
         self.ensure_one()
         if args['headers']['action'] == 'status_logout':
-            args['data'] = json.dumps({})  # backwards compatibility
+            args.pop('data', None)
         return args
 
     def ca_request(self, path, data={}, params={}, timeout=False, ignore_exception=False):
